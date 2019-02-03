e6015e36229fa17210905a55830813dd5125fd68
Felipe Caon <felipecaon@gmail.com>
2019-02-03 17:23:05 -0200

    receive username as argument

--- comperio.py
+++ comperio.py
@@ -1,21 +1,34 @@
 import requests
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-username = 'blackocean'
 f = open("sites.txt", "r")
 found_profiles_file = open("found_profiles.txt", "w+")
 found_profiles_file.truncate()
 
 
 def main():
-    for url in f:
-        formatted_url = url.format(username)
-        if(requests.get(formatted_url).status_code == 200):
-            found_profiles_file.write(formatted_url)
 
-    found_profiles_file.seek(0)
-    for profile in found_profiles_file:
+    parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter)
+
+    parser.add_argument("username",
+                        nargs='+', metavar='USERNAMES',
+                        action="store",
+                        help="One or more usernames"
+                        )
+
+    args = parser.parse_args()
+
+    for username in args.username:
+        print('Now trying: ' + username)
+        for url in f:
+            formatted_url = url.format(username)
+            if(requests.get(formatted_url).status_code == 200):
+                found_profiles_file.write(formatted_url)
+
+        found_profiles_file.seek(0)
         print('Found profiles:\n')
-        print(profile)
+        for profile in found_profiles_file:
+            print(profile)
 
 
 if __name__ == "__main__":
